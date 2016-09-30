d54b4590c75bfac0eb6a205d472c7d0ced2f0527
Hunter Damron <hdamron1594@yahoo.com>
2016-09-30 13:54:26 -0400

    SOme simple stuff.

---
+++ pendulum/main.py
@@ -0,0 +1,19 @@
+'''
+Demonstrates numerical integration of the motion of linear damped driven 
+    pendulums
+Created on Sep 30, 2016
+@author: Hunter Damron
+'''
+
+def dw_dt(natural_freq, friction_coef, damping_freq, damping_torque):
+    """
+    Derivative of omega (angular velocity)
+    :param natural_freq: natural frequency of ideal pendulum
+    :param friction_coeff: coefficient of friction force
+    :param damping_freq: frequency of damping force
+    :param damping_torque: torque due to damping force
+    :param return: returns instantaneous acceleration
+    """
+
+if __name__ == '__main__':
+    pass
